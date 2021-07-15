dfeae1b8f7eb89f09402af847bded16aabe27e2c
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-15 21:22:28 +0200

    navigation style added

--- src/App.js
+++ src/App.js
@@ -2,34 +2,34 @@ import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 import AuthorPage from "./features/author/AuthorPage";
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
-import { StyledNavLink } from "./styled";
+import { Item, List, StyledNav, StyledNavLink } from "./styled";
 
 const App = () => (
     <HashRouter>
-        <nav>
-            <ul>
-                <li>
+        <StyledNav>
+            <List>
+                <Item>
                     <StyledNavLink to="/zadania">Zadania</StyledNavLink>
-                </li>
-                <li>
+                </Item>
+                <Item>
                     <StyledNavLink to="/autor">O autorze</StyledNavLink>
-                </li>
-            </ul>
-            <Switch>
-                <Route path="/zadania/:id">
-                    <TaskPage />
-                </Route>
-                <Route path="/zadania">
-                    <TasksPage />
-                </Route>
-                <Route path="/autor">
-                    <AuthorPage />
-                </Route>
-                <Route path="/">
-                    <Redirect to="/zadania" />
-                </Route>
-            </Switch>
-        </nav>
+                </Item>
+            </List>
+        </StyledNav>
+        <Switch>
+            <Route path="/zadania/:id">
+                <TaskPage />
+            </Route>
+            <Route path="/zadania">
+                <TasksPage />
+            </Route>
+            <Route path="/autor">
+                <AuthorPage />
+            </Route>
+            <Route path="/">
+                <Redirect to="/zadania" />
+            </Route>
+        </Switch>
     </HashRouter>
 );
 
--- src/styled.js
+++ src/styled.js
@@ -3,10 +3,28 @@ import styled from "styled-components";
 
 const activeClassName = "active";
 
+export const StyledNav = styled.nav`
+    background-color: ${({ theme }) => theme.colors.primaryColor};
+`;
+
+export const List = styled.ul`
+    list-style: none;
+    display: flex;
+    margin: 0 auto;
+    justify-content: center;
+`;
+
+export const Item = styled.li`
+    padding: 20px;
+`;
+
 export const StyledNavLink = styled(NavLink).attrs(() => ({
     activeClassName,
 }))`
+    text-decoration: none;
+    color: ${({ theme }) => theme.colors.textColor};
+
     &.${activeClassName}{
-        color: red;
+        font-weight: bold;
     }
 `;
