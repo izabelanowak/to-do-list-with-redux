697408c07ccd0643302aa23f2845b69f71a3b65f
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-07-16 13:59:03 +0200

    font-size changed

--- src/features/tasks/TasksPage/Button/index.js
+++ src/features/tasks/TasksPage/Button/index.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Button = styled.button`
     background: transparent;
+    font-size: 14px;
     color: ${({ theme }) => theme.colors.primaryColor};
     border: none;
     cursor: pointer;
